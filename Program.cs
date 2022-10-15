ab41a4400e17d70837014f823c4970a589e960ff
Tak <61429464+Jokuuur@users.noreply.github.com>
2022-10-15 11:33:18 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿/*//Zad.1
+Console.WriteLine("Podaj liczbę a : ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Podaj liczbę b : ");
+int b = int.Parse(Console.ReadLine());
+if ((a+b)%2==0)
+{
+    Console.WriteLine("Parzyste");
+}
+else
+{
+    Console.WriteLine("Nie Parzyste");
+}*//*
+//Zad.2
+Console.WriteLine("Podaj liczbę a : ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Podaj liczbę g : ");
+int g = int.Parse(Console.ReadLine());
+if((a+g)/2 > Math.Sqrt(a*g))
+{
+    Console.WriteLine("Średnia Arytemtyczna jest większa ");
+}
+else
+{
+    Console.WriteLine("Średnia Geometryczna jest większa ");
+}
+//Zad.3*//*
+Console.WriteLine("Podaj k : ");
+int k = int.Parse(Console.ReadLine());
+Console.WriteLine("Podaj l : ");
+int l = int.Parse(Console.ReadLine());
+Console.WriteLine("Podaj m : ");
+int m = int.Parse(Console.ReadLine());
+if (k == l || l == m || m == k)
+{
+    Console.WriteLine("TAK");
+}
+else
+{
+    Console.WriteLine("NIE");
+}
+if (k ==l)
+    Console.WriteLine("Te liczby to k = l ");
+if (l == m)
+    Console.WriteLine("Te liczby to l = m");
+if (m == k)
+    Console.WriteLine("Te liczby to m = k");*//*
+//Zad.4
+*//*Console.WriteLine("Podaj liczbę a : ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Podaj liczbę b : ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Podaj liczbę c : ");
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine("Podaj liczbę d : ");
+int d = int.Parse(Console.ReadLine());
+*//*
+//Zad.5
+Console.WriteLine("Podaj najkrótszy bok : ");
+int krotki = int.Parse(Console.ReadLine());
+Console.WriteLine("Podaj bok większy od poprzedniego ale nie najdłuższy : ");
+int sredni = int.Parse(Console.ReadLine());
+Console.WriteLine("Podaj najdłuższy bok");
+int dlugi = int.Parse(Console.ReadLine());
+if ((krotki + sredni)>dlugi)
+{
+    Console.WriteLine("Taki trójkąt może zaistnieć");
+}
+if((krotki + sredni)<dlugi)
+{
+    Console.WriteLine("Taki trójkąt nie zaistnieje");
+}*/
